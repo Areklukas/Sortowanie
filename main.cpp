0658c21711f544ded45aee536405960b86e5b5dc
Areklukas <56909985+Areklukas@users.noreply.github.com>
2020-03-27 13:09:51 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+int a;
+int i;
+int main()
+{
+    string(w) = "abacbede";
+
+        for (int a=0; a<w.length()-1; a++)
+
+        for (int i=0; i<w.length()-1; i++)
+
+        if (w[a]>w[a+1])
+
+        swap(w[a], w[a+1]);
+
+
+    cout <<"Sortowanie babelkowe:" <<endl;
+    cout <<w <<endl;
+
+return 0;
+}
